ac7094912ff88a0af68d7795e958283df578f0b1
Jakub Wąsala <jakubwasala1@gmail.com>
2022-01-09 12:41:47 +0100

    created Category model

---
+++ lib/models/category.dart
@@ -0,0 +1,30 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
+
+class Category {
+  final int categoryId;
+  final String name;
+
+  Category(this.categoryId, this.name);
+}
+
+class CategoryModel extends ChangeNotifier {
+  final List<Category> _categories = [
+    Category(1, 'Warzywa'),
+    Category(2, 'Owoce'),
+    Category(3, 'Bakalie')
+  ];
+
+  Category? find(int id) {
+    for (var category in _categories) {
+      if (category.categoryId == id) {
+        return category;
+      }
+    }
+    return null;
+  }
+
+  UnmodifiableListView<Category> get categories =>
+      UnmodifiableListView(_categories);
+}
